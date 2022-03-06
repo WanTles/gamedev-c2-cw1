8c3e89670a93c95c45253781fe1cf429fdb3ac0d
WanTles <mohammadashknani953@gmail.com>
2022-03-06 13:11:20 +0300

    programed a cod
    so that the player tells you his speed,age,super power 1 and super power2

---
+++ cw2-cw1/cw2-cw1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace cw2_cw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string charcterName = "crash";
+
+            string superPower1 = "high jump";
+
+            string superPower2 = "run fast";
+
+            int age = 100;
+
+            double speed = 5.40;
+
+            Console.WriteLine("my name is {0} and my super power1 is {1} my super power 2 is {2} my age is {3} my speed is {4}", charcterName, superPower1, superPower2, age, speed);
+        }
+    }
+}
